f9a5961e0dcf7b9927d0726f4ee75abf7af2c3a3
Yuriy Bevov <jury19y@mail.ru>
2019-02-22 01:24:30 +0300

    22.02

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var search = document.querySelector(".button-search");
+var form = document.querySelector(".hotel-searching");
+
+  search.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    form.classList.toggle("visually-hidden");
+  });
