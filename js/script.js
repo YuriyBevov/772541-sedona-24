2c812f11392e10470d29127a7cf068d1063df163
Yuriy Bevov <jury19y@mail.ru>
2019-03-10 19:30:36 +0300

    чекбоксы

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 var search = document.querySelector(".button-search");
 var form = document.querySelector(".hotel-searching");
 
-  form.classList.add("hidden");
-  
-  search.addEventListener("click", function(evt) {
+form.classList.add("hidden");
+
+search.addEventListener("click", function(evt) {
     evt.preventDefault();
     form.classList.toggle("form-show");
-  }
-);
+});
