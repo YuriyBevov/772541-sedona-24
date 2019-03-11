3fa9b2f04ba3933894d4277cead2fd956fc6e413
Yuriy Bevov <jury19y@mail.ru>
2019-03-11 21:25:57 +0300

    защита2

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 var search = document.querySelector(".button-search");
 var form = document.querySelector(".hotel-searching");
+var checkIn = form.querySelector("[name=check-in]");
+var checkOut = form.querySelector("[name=check-out]");
+var adults = form.querySelector("[name=adults]");
 
 form.classList.add("hidden");
 
 search.addEventListener("click", function(evt) {
     evt.preventDefault();
     form.classList.toggle("form-show");
+    form.classList.remove("form-error");
+});
+
+form.addEventListener("submit",function(evt){
+    if(!checkIn.value || !checkOut.value) {
+        evt.preventDefault();
+        form.classList.add("form-error");
+  }
 });
